258ec8a82bcb3bf2c4b39a65d42a40799dc3a698
ahmed <bos-violation@stc.com.sa>
2021-03-27 22:37:19 +0300

    explore different type of operators

---
+++ src/com/ahmed/chapter2/MathOperators.java
@@ -0,0 +1,73 @@
+package com.ahmed.chapter2;
+
+public class MathOperators {
+
+    public static void main(String[] args) {
+        System.out.println(" basic math operators");
+        basicMath();
+        compoundAssignment();
+        mathFunctions();
+    }
+
+    private static void basicMath(){
+        int init = 11;
+        int split = 3;
+        int partA = init / split;
+        System.out.println(" 11 / 3 = "+ partA);
+        int partB = init % split;
+        System.out.println(" 11 % 3 = "+ partB);
+        int result = split * (partB + partA);
+        System.out.println(" 3 * (3 + 2) = "+ result);
+    }
+
+    private static  void compoundAssignment(){
+        System.out.println(" compound Assignment operators");
+        int a = 1, b = 3;
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+    }
+
+    private static void mathFunctions(){
+        System.out.println("Math class functions ");
+        double a = 1.99, b = 2.99;
+        //"NaN" stands for "not a number" and signifies that a value is not defined.
+        double cosine = Math.cos(a);
+        System.out.println("cosine ="+ cosine);
+        double arcCosine = Math.acos(cosine);
+        System.out.println("arc cosine ="+ arcCosine);
+        double sine = Math.sin(a);
+        System.out.println("sine ="+ sine);
+        double arcSine = Math.asin(sine);
+        System.out.println("arc sine ="+ arcSine);
+
+        double tangent = Math.tan(a);
+        System.out.println("Tangent = "+ tangent);
+
+        double arcTangent = Math.atan(tangent);
+        System.out.println("arc tangent = "+ arcTangent);
+
+
+        double exp = Math.exp(a);
+        System.out.println("e^1.99 = "+ exp);
+
+        double max = Math.max(a, b);
+        System.out.println("max between 1.99 & 2.99 = "+ max);
+
+        double min = Math.max(a, b);
+        System.out.println("min between 1.99 & 2.99 = "+ min);
+
+
+        double pow = Math.pow(a, b);
+        System.out.println("a^b = "+pow);
+
+        double sqrt = Math.sqrt(a);
+        System.out.println("Square root of a = " + sqrt);
+
+        double random = Math.random();
+        System.out.println("random number between 0.0 and 1.0: "+ random);
+    }
+
+}
--- src/com/ahmed/chapter2/Operators.java
+++ src/com/ahmed/chapter2/Operators.java
@@ -2,5 +2,108 @@ package com.ahmed.chapter2;
 
 public class Operators {
 
+    //TODO
+    // List of operators precedence:
+    // 1- postfix  expr++ expr--
+    // 2- unary  ++expr --expr  +expr ~ !
+    // 3- multiplicative   * / %
+    // 4- additive  + -
+    // 5- shift << >> >>>
+    // 6- relational < > <= >= instanceof
+    // 7- equality == !=
+    // 8- bitwise AND &
+    // 9- bitwise exclusive OR  ^
+    // 10- bitwise inclusive OR |
+    // 11- logical AND &&
+    // 12- logical OR ||
+    // 13- tenary ?:
+    // 14- assignment = += -= *= /= %= &= ^= |= <<= >>= >>>=
+
+
+    public static void main(String[] args) {
+        System.out.println("postfix and prefix");
+        postfixAndPrefixOperator();
+        System.out.println("shift");
+        shiftOperator();
+        System.out.println(" bitwise AND & , bitwise exclusive OR ~ , bitwise inclusive OR |  operators:");
+        bitwiseORandAND();
+
+    }
+
+
+
+    private static void bitwiseORandAND(){
+        // & 1 1  ->   1
+        // & 1 0  ->  0
+
+
+        // | 1 1  -> 1
+        // | 1 0  ->  1
+        // | 0  0 -> 0
+
+
+        int a = 25;  // 1 1 0 0 1
+        int b = 15;  // 0 1 1 1 1
+        int and = a & b; //0 1 0 0 1 -> 9
+        System.out.println("bitwise and operator  25 & 15 = "+ and);
+
+        // similar will be True(1) and dissimilar will be False(0)
+        int inclusiveOR = a | b;  // 1 1 1 1 1 -> 31
+        System.out.println("bitwise inclusive or operator  25 | 15 = "+ inclusiveOR);
+
+
+        // similar will be False(0) and dissimilar will be True(1)
+       int exclusiveOR = a ^ b; // 1 0 1 1 0
+        System.out.println("bitwise exclusive or operator 25 ^ 15 = "+ exclusiveOR);
+    }
+
+
+
+
+
+
+    private static  void postfixAndPrefixOperator (){
+        int i = 1,postfix = i++, prefix = ++i ;
+        System.out.println("Postfix is assigned first then incremented so postfix = "+ postfix);
+        System.out.println("Prefix is incremented first then assigned so prefix  = "+ prefix);
+    }
+
+
+    private static void  shiftOperator(){
+        byte x, y;
+        x=10;
+        y=-10;
+        System.out.println("Bitwise Left Shift: x<<2 = "+(x<<2));
+        System.out.println("Bitwise Right Shift: x>>2 = "+(x>>2));
+        System.out.println("Bitwise Zero Fill Right Shift: x>>>2 = "+(x>>>2));
+        System.out.println("Bitwise Zero Fill Right Shift: y>>>2 = "+(y>>>2));
+    }
+
+
+
+
+
+    private static int question3(){
+        int x = 1, y = 1, z = 0;
+        if (x == y | x < ++y) {
+                //pay attention to ++y
+            // z = 1 + 2
+            z = x+y;
+            System.out.println(z);
+        }
+        else{
+            z = 1;
+        }
+        return z;
+    }
+
+
+
+
+
+
+
+
+
 
 }
